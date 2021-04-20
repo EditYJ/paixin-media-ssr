2bc3a5adfb0a20f8de9a22c1f7cfe5060defc9d7
EditYJ <158392613@qq.com>
2021-04-20 14:38:59 +0800

    init: 使用环境变量

--- web/utils/service.ts
+++ web/utils/service.ts
@@ -2,19 +2,17 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:3000/',
+  baseURL: process.env.BASE_URL,
   timeout: 3000,
 })
 
 service.interceptors.response.use(
   response => {
-    console.log('自定义环境变量', process.env.BASE_URL)
     if (response.status === 200) {
       return response.data
     }
   },
   async error => {
-    console.log('自定义环境变量', process.env.BASE_URL)
     if (__isBrowser__) {
       ElMessage({
         type: 'error',
