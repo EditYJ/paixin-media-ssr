7447bb2632e92f5b60c867fa9203341d8d9d56bd
EditYJ <158392613@qq.com>
2021-04-22 18:01:16 +0800

    整理代码

--- web/pages/index/render.vue
+++ web/pages/index/render.vue
@@ -1,21 +1,12 @@
 
 <template>
-  <div>
-    <h1>{{ isLoading }}</h1>
+  <div class="home">
+    <h1 v-if="isLoading">加载中...</h1>
     <Search />
-    <template v-if="indexData">
-      <el-button @click="testClick"> 我是element按钮index </el-button>
-      <div class="test-block">hahaha</div>
-      <input type="text" />
-      <img src="/images/expired-coupon-dialog.png" alt="" />
-      <pre>{{ indexData }}</pre>
-    </template>
-    <template v-else>
-      <img
-        src="https://gw.alicdn.com/tfs/TB1v.zIE7T2gK0jSZPcXXcKkpXa-128-128.gif"
-        class="loading"
-      />
-    </template>
+    <el-button @click="testClick"> 我是element按钮index </el-button>
+    <div class="test-block">hahaha</div>
+    <img class="test-img" src="/images/expired-coupon-dialog.png" alt="" />
+    <pre>{{ indexData }}</pre>
   </div>
 </template>
 
@@ -47,12 +38,17 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.test-block {
-  font-size: 16px;
-  color: $color-primary;
-  background-color: gray;
-  width: 250px;
-  height: 250px;
-  @include box-center;
+.home {
+  .test-block {
+    font-size: 16px;
+    color: $color-primary;
+    background-color: gray;
+    width: 250px;
+    height: 250px;
+    @include box-center;
+  }
+  .test-img{
+    width: 400px;
+  }
 }
 </style>
